15235ebd68b7b1cdbc008fb170b751ea8a40a002
kwesimain <opokulive@gmail.com>
2024-05-18 23:30:53 +0000

    added new models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,5 +1,9 @@
+# models.py
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -26,10 +30,21 @@ class Customer(models.Model):
     def __str__(self):
         return self.user.username
 
+@receiver(post_save, sender=User)
+def create_customer(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+    # Save the Customer object whenever the associated User object is saved
+    instance.customer.save()
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
+
+    # Add a field to store the total price of the order
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return str(self.id)
